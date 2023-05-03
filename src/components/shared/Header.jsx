9411fe2a3dd2c58f40cf10291243c734187da779
Anik <naimur.rezaa@gmail.com>
2023-05-03 20:17:59 +0600

    added burger menu and responsive header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
 import { AuthContext } from "../../providers/AuthProvider/AuthProvider";
+import { Fade as Hamburger } from "hamburger-react";
+
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
   const { user, logOut } = useContext(AuthContext);
   console.log(user);
   return (
@@ -14,8 +17,12 @@ const Header = () => {
           </NavLink>
         </div>
 
-        <div className="navbar-end">
-          <div className="space-x-5 font-semibold text-white">
+        <div
+          className={`marker:lg:navbar-end absolute lg:flex-row flex-col items-center  flex z-10 duration-500  w-40 rounded-br-lg rounded-bl-lg   p-7 lg:p-0 lg:h-auto bg-green-200 lg:static ${
+            isOpen ? "top-0" : "-top-96"
+          }`}
+        >
+          <div className="space-x-0 lg:space-x-5 space-y-2  lg:space-y-0  font-semibold text-white flex flex-col lg:flex-row">
             <NavLink
               to={"/home/"}
               className={({ isActive }) =>
@@ -55,7 +62,7 @@ const Header = () => {
               className="dropdown tooltip tooltip-left tooltip-warning "
             >
               {" "}
-              <button className="  btn btn-circle avatar online   btn-outline ml-3">
+              <button className=" btn btn-circle avatar online   btn-outline lg:ml-3">
                 <img
                   className="rounded-full ring-4"
                   src={user?.photoURL}
@@ -64,7 +71,7 @@ const Header = () => {
               </button>
               <ul
                 tabIndex={0}
-                className="dropdown-content  menu p-2 shadow bg-black bg-opacity-70 rounded-box w-fit"
+                className="dropdown-content   menu p-2 shadow bg-black bg-opacity-70 rounded-box w-fit"
               >
                 <li>
                   <NavLink className="text-white" to={"/profile"}>
@@ -81,11 +88,14 @@ const Header = () => {
           ) : (
             <NavLink
               to={"/login"}
-              className=" btn btn-circle btn-outline ml-3 text-white"
+              className=" btn btn-circle btn-outline lg:ml-3 mt-3 mr-2 lg:mr-0 lg:mt-0 text-white"
             >
               <FaUserAlt className="" />
             </NavLink>
           )}
+        </div>
+        <div className="ml-auto">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
       </div>
     </div>
