bf1c697bd5ca0c5060bcd0d92db10cbe66e27a16
Anik <naimur.rezaa@gmail.com>
2023-05-13 21:07:20 +0600

    test

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBnCvkcJlwXKrvVaSU9G6BoxzKOOnFCDYI",
-  authDomain: "chef-master-pro.firebaseapp.com",
-  projectId: "chef-master-pro",
-  storageBucket: "chef-master-pro.appspot.com",
-  messagingSenderId: "711965413473",
-  appId: "1:711965413473:web:92297694bf29a8c2365a20",
-  measurementId: "G-S0X7ZX4P82",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 // Initialize Firebase
